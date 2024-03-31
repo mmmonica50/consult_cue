e8cc4342de1560eb54ecedf87027b1db37d1ec02
mmmonica50 <mmmonica50@gmail.com>
2024-03-31 12:44:24 -0600

    Feat: Workshop page content type

---
+++ src/api/workshop-detail/controllers/workshop-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * workshop-detail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::workshop-detail.workshop-detail');
